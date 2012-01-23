0f857c98b22c0250745d1bd2c2f563be11826cc1
Benedikt Boss <vork.android@gmail.com>
2012-01-24 00:01:36 +0100

    This isn't needed (and not there anymore)

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -21,7 +21,6 @@ TARGET_SPECIFIC_HEADER_PATH := device/lge/star-common/include
 
 BOARD_HAS_NO_SELECT_BUTTON := true
 
-BOARD_USES_GENERIC_AUDIO := true
 BOARD_USES_AUDIO_LEGACY := true
 # Use dirty hack to allow froyo libaudio
 
